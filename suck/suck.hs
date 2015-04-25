794fc9b7ac53b15462f4666431ba6a59d370f30a
Lee Ehudin <percivalgambit@gmail.com>
2015-04-25 18:10:07 -0500

    suck can now retrieve webpages from urls.txt and partially strip off tags

--- suck/suck.hs
+++ suck/suck.hs
@@ -1,4 +1,25 @@
 module Main where
 
+import Paths_haskal
+
+import Control.Monad
+import Data.Map
+import Network.HTTP
+import System.IO
+import Text.HTML.TagSoup
+
+type PrimitiveModel = Map (String,String) [String]
+type ProcessedModel = [(String,[(Int,Int)])]
+
 main :: IO ()
-main = return ()
+main = do
+    urls <- getDataFileName "urls.txt"
+    urlList <- lines <$> readFile urls
+    pages <- getPages urlList
+    let pageTexts = (innerText . parseTags) <$> pages
+    return ()
+
+getPages :: [String] -> IO [String]
+getPages urlList = do
+    let requests = (simpleHTTP . getRequest) <$> urlList
+    mapM (>>= getResponseBody) requests
